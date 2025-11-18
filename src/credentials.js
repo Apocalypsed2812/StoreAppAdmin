1157a0cc4a5eebecbe6c0af3928e79671b3fdca3
Apocalysed2812 <tien.pham@omn1solution.com>
2025-11-18 23:13:58 +0700

    edit server API

--- src/credentials.js
+++ src/credentials.js
@@ -4,4 +4,5 @@ export const TOKEN_NAME = 'token_user_vt_ecommerce_name';
 //export const SERVER_API = 'http://localhost:5000/api/';
 
 // export const SERVER_API = 'https://food-shop-api.herokuapp.com/api/';
-export const SERVER_API = 'https://food-app-api.vercel.app/api/';
+// export const SERVER_API = 'https://food-app-api.vercel.app/api/';
+export const SERVER_API = 'https://foodappnodeapi.onrender.com/api/';
